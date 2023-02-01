56d364c9bfb2c82249400cff73133cb5c9a8d0d8
choihh@farmingdale.edu <swami@137.125.147.134>
2023-01-31 22:04:19 -0500

    rendered get and set methods for each members

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -3,8 +3,6 @@
  Complete the followings:
 
 
- - You need to have setters and getters for all data members
- - You must have a default constructor and an overloaded one that accepts 3 parameters to update the member variables.
 
  - Using a driver class (with main), instantiate the Course class and call its methods to change the id, name, and code.
 
@@ -31,24 +29,49 @@ public class Course {
      * The Course class should have:
     - ID, Name, Code
      */
-    int ID;
-    String name;
-    int code;
+    private int ID;
+    private String name;
+    private int code;
     
-    int getID(){
-        return ID;
+    /**
+     *  You must have a default constructor and an overloaded one that 
+     *  accepts 3 parameters to update the member variables.
+     */
+    
+    
+    
+    /**
+     * You need to have setters and getters for all data members
+     */
+    
+    public int getID(){
+        return this.ID;
     }
     
-    String getName(){
-        return name;
+    public void setID(int ID){
+        this.ID = ID;
     }
     
-    int getCode(){
-        return code;
+    public String getName(){
+        return this.name;
     }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public int getCode(){
+        return this.code;
+    }
+    
+    public void setCode(int code){
+        this.code = code;
+    }
+    
+}
     
     
     
     
 
-}
+
